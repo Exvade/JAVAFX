bb8680711ee4397b976805594b81742fa4625b1f
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-05-05 22:31:48 +0700

    insialisasi project crud java

---
+++ crudJava/src/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+  try {
+   Object root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+   Scene scene = new Scene((Parent) root);
+   primaryStage.setTitle("CRUD JAVA");
+   primaryStage.setScene(scene);
+   primaryStage.show();
+  } catch (Exception e) {
+    // TODO: handle exception
+  }
+}
+
+public static void main(String[] args) {
+ launch(args);
+}
+}
